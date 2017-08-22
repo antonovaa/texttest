60007162ce3131ba4748e47c91a45f036f3eb1ee
antonovaa <antonovaa2904@gmail.com>
2017-08-22 18:29:13 +0300

    fix)

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -3,48 +3,51 @@
 
 #include "stdafx.h"
 #include <string>
-#include <fstream>
 #include <iostream>
+#include <fstream>
+#include <vector>
 
 using namespace std;
 
 int main(int argc, char* argv[])
 {
-	string adr= "E:\\Users\\ddd.txt";
-	char buff[50];
-	int i;
-	int k = 0;
+	string adr= "E:\\Users\\alice.txt";
+	string str, tmp;
+	vector<string> vec;
+	int k;
+	k = 0;
 	if (argc >= 2)// если есть адрес и параметр
 	{
 		
-		adr = argv[1];
+		//adr = argv[1];
 		
-		try {
 
-			 
-			for (int i = 2; i < argc; i++) //для каждого слова. название и адрес файла пропускаем, начинаем поиск параметров с 2
-				{
-				ifstream fin(adr);// открыли файл для чтения
-				fin >> buff;
-				while(!fin.eof()); //пока считываем
-				{
-					if (argv[i] == buff); //одинаковы ли
-					{
-						k++;
-					}
-					fin >> buff;
-				}
-				cout << argv[i] << " найдено " << k << " раз";
-				k == 0;
-				fin.close(); // закрываем файл
+			ifstream fin(adr);
+			while (fin >> tmp)
+			{
+				vec.push_back(tmp);
 			}
-			
-			
-		}
-		catch (int)
-		{
 
-		}
+			int  min = vec[0].size();
+			for (int i = 2; i < argc; i++) //для каждого слова. название и адрес файла пропускаем, начинаем поиск параметров с 2
+			{
+				string str(argv[i]);
+				for (size_t l = 0; l != vec.size(); l++)
+				{
+					if (str==vec[l]) //одинаковы ли
+					{
+						//cout << argv[i] << "  " << vec[l]<<endl;
+						k++;
+					}	
+					
+				}
+				cout << argv[i] << " found " << k << " times";
+				k = 0;
+			}
+				fin.close(); // закрываем файл
+			
+			
+
 	}
 	system("pause");
 	return 0;
